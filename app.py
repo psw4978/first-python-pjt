78d8d90da9557a2d2605633939aa66c7da80b5b6
psw4978 <psw4978@naver.com>
2023-03-22 19:49:43 +0900

    현대,oci 가격 등락 프로그램

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+파일 = open('b.txt', 'w')
+파일.write('삼성전자')
+파일.close()
 
+파일 = open('b.txt', 'a')
+파일.write ('\n카카오\n네이버\n신풍제약')
+파일.close()
 
-def 함수(x) : 
-    for i in range(0,x) :
-     print(i,"번 입니다.")
-    else : print("탈락")
+파일 = open('b.txt', 'r')
+print (파일.read())
+파일.close()
 
-함수(10)
 
---
+++ naver/app.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+현대에너지솔루션데이터 = requests.get('https://finance.naver.com/item/sise.naver?code=322000')
+
+현대에너지솔루션soup = BeautifulSoup(현대에너지솔루션데이터.content, 'html.parser')
+no_up = 현대에너지솔루션soup.find_all('span', class_='blind')[12]
+rate = 현대에너지솔루션soup.select('.no_up span')
+print(no_up.text,rate[14].text,rate[15].text,rate[20].text)
+
+OCI데이터 = requests.get('https://finance.naver.com/item/main.naver?code=010060')
+
+OCIsoup = BeautifulSoup(OCI데이터.content, 'html.parser')
+no_up = OCIsoup.find_all('span', class_='blind')[12]
+rate = OCIsoup.select('.no_up span')
+print(no_up.text,rate[15].text,rate[16].text,rate[21].text)
+
+
+
