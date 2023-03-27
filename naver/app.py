86ecf597129892047d6981564232796aaa80c060
psw4978 <psw4978@naver.com>
2023-03-27 18:04:23 +0900

    230327 6시

--- naver/app.py
+++ naver/app.py
@@ -5,15 +5,14 @@ from bs4 import BeautifulSoup
 
 현대에너지솔루션soup = BeautifulSoup(현대에너지솔루션데이터.content, 'html.parser')
 no_up = 현대에너지솔루션soup.find_all('span', class_='blind')[12]
-rate = 현대에너지솔루션soup.select('.no_up span')
+rate = 현대에너지솔루션soup.select('.no_down span')
 print(no_up.text,rate[14].text,rate[15].text,rate[20].text)
 
 OCI데이터 = requests.get('https://finance.naver.com/item/main.naver?code=010060')
 
 OCIsoup = BeautifulSoup(OCI데이터.content, 'html.parser')
 no_up = OCIsoup.find_all('span', class_='blind')[12]
-rate = OCIsoup.select('.no_up span')
-print(no_up.text,rate[15].text,rate[16].text,rate[21].text)
-
+rate = OCIsoup.select('.no_down span')
+print(no_up.text,rate[13].text,rate[14].text,rate[19].text)
 
 
